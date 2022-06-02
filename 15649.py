3a447377428d41fd4f847c60f6ae0ad3ebd127bb
hyeyul-dev <kimmisun9283@naver.com>
2022-06-02 19:02:58 +0900

    [Fix] indent

--- 15649.py
+++ 15649.py
@@ -8,15 +8,14 @@ rs = []
 chk = [False] * (n+1)
 
 def recur(num):
-	if num == m:
-    	print(' '.join(map(str,rs)))
+    if num == m:
+        print(' '.join(map(str,rs)))
         return
     for i in range(1,n+1):
-    	if chk[i] == False:
-        	chk[i] = True
-        	rs.append(i)
+        if chk[i] == False:
+            chk[i] = True
+            rs.append(i)
             recur(num+1)
             chk[i] = False
             rs.pop()
-            
 recur(0)
